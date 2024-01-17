d50a1921da324b2447dd76d25215a16ada9cd173
ardigiandi <ardiansyahg9080@gmail.com>
2024-01-17 13:18:40 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        ungu : '#7839F3',
+        ijo : '#31B380',
+        transparan : '#ECE3FE',
+      }
+    },
+  },
+  plugins: [],
+}
+
